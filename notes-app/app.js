e40950a40cdd1184eef9e831c8ad5e4974894da6
German Valencia Vargas <sttamper@gmail.com>
2026-02-18 20:53:36 -0500

    using npm

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
+const validator = require('validator')
 const { name, add } = require('./utils.js');
 const getNotes = require('./notes.js');
 
@@ -5,3 +6,4 @@ const sum = add(5, 10);
 console.log(name);
 console.log(sum);
 console.log(getNotes());
+console.log(validator.isEmail('test@example.com'));
