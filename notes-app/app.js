64c14134dbc1d3944eb2dd93f89555ec42ac183e
German Valencia Vargas <sttamper@gmail.com>
2026-02-18 20:56:13 -0500

    challenge: using another npm

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,11 @@
 const validator = require('validator')
 const { name, add } = require('./utils.js');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 const sum = add(5, 10);
 console.log(name);
 console.log(sum);
 console.log(getNotes());
 console.log(validator.isEmail('test@example.com'));
+console.log(chalk.bold.green('Success!'));
